769e68a8e03f54d99a8c4fc2c35f40576800c987
Taynara Pecorario <taynara_rp22@hotmail.com>
2020-08-18 22:14:01 -0300

    beggining of register patient form

--- src/pages/NewOccurrence/index.js
+++ src/pages/NewOccurrence/index.js
@@ -42,6 +42,61 @@ export default function NewOccurrence() {
         </div>
       </div>
 
+      <main>
+        <p>PÁGINA INICIAL / NOVA OCORRÊNCIA</p>
+
+        <h2>Nova ocorrência</h2>
+
+        <form className="patient-register">
+          <fieldset>
+            <legend>Cadastro do paciente</legend>
+
+            <label for="name">Nome completo</label>
+            <input type="text" id="name" name="name" placeholder="DIGITE O NOME DO PACIENTE" />
+
+            <label for="cpf">CPF</label>
+            <input type="text" id="cpf" name="cpf" placeholder="DIGITE O CPF DO PACIENTE" />
+
+            <label for="age">Idade</label>
+            <input type="text" id="date" name="date" placeholder="DIGITE A IDADE" />
+
+            <label for="weight">Peso</label>
+            <input type="text" id="weight" name="weight" placeholder="DIGITE O PESO" />
+
+            <label for="gender">Gênero</label>
+            <select id="gender" name="gender" >
+              <option value="Feminino">Feminino</option>
+              <option value="Masculino">Masculino</option>
+              <option value="Outro">Outro</option>
+            </select>
+
+            <label for="emercency-contact-name">Nome do contato de emergência</label>
+            <input type="text" id="emercency-contact-name" name="emercency-contact-name" placeholder="DIGITE O NOME DO CONTATO" />
+
+            <label for="emercency-contact-telephone">Telefone do contato de emergência</label>
+            <input type="number" id="emercency-contact-telephone" name="emercency-contact-telephone" placeholder="DIGITE O TELEFONE" />
+
+            <label for="cep">CEP da ocorrência</label>
+            <input type="number" id="cep" name="cep" placeholder="DIGITE O CEP" />
+
+            <label for="adress">Endereço da ocorrência</label>
+            <input type="text" id="adress" name="adress" placeholder="NOME DA RUA" />
+
+            <label for="adress-number">Número</label>
+            <input type="text" id="adress-number" name="adress-number" placeholder="NÚMERO DA RESIDÊNCIA" />
+
+            <label for="state">Estado</label>
+            <select id="state" name="state">
+            </select>
+
+            <label for="city">Cidade</label>
+            <select id="city" name="city">
+            </select>
+          </fieldset>
+        </form>
+        
+      </main>
+
     </div>
   );
 }
